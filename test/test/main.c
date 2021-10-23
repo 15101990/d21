b04c3b174dd8f4d0fa267ca650bc2ef22ba42ff2
15101990 <m.helmy.amit@gmail.com>
2021-10-23 19:29:13 +0200

    jjj

---
+++ test/test/main.c
@@ -0,0 +1,20 @@
+/*
+ * test.c
+ *
+ * Created: 22/10/2021 09:24:57 ص
+ * Author : dell
+ */ 
+
+#include <avr/io.h>
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+int main(void)
+{
+   u32 x = 20;
+   SET_BIT(x,0);
+    while (1) 
+    {
+    }
+}
+
